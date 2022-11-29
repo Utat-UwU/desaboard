29d8b962168b71125f29677b9e444a948b202c4c
Utat-UwU <putra2prasetyo@apps.ipb.ac.id>
2022-11-29 19:30:01 +0700

    Update tambahdata.blade.php

--- resources/views/data/tambahdata.blade.php
+++ resources/views/data/tambahdata.blade.php
@@ -14,31 +14,54 @@
             @csrf
             <div class="form-group mb-3">
                 <label for="NIK">NIK</label>
-                <input class="form-control" type="text" id="NIK" placeholder="NIK"  required autofocus name="NIK"> 
-
+                <input class="form-control @error('NIK') is-invalid @enderror" type="text" id="NIK" placeholder="NIK" required autofocus name="NIK"> 
+                @error('NIK') 
+                <div class="invalid-feedback">
+                {{ $message }}
+                </div>
+                @enderror
             </div>
+
             <div class="form-group mb-3">
                 <label for="Nama">Nama Lengkap</label>
-                <input class="form-control" type="text" id="Nama" placeholder="Nama Lengkap" required autofocus name="Nama"> 
+                <input class="form-control @error('Nama') is-invalid @enderror" type="text" id="Nama" placeholder="Nama Lengkap" required autofocus name="Nama"> 
+                @error('Nama') 
+                <div class="invalid-feedback">
+                {{ $message }}
+                </div>
+                @enderror
             </div>
+
             <div class="form-group mb-3">
                 <label for="Nomor_KK">Nomor KK</label>
-                <input class="form-control" type="text" id="Nomor_KK" placeholder="Nomor KK" required autofocus name="Nomor_KK"> 
+                <input class="form-control @error('Nomor_KK') is-invalid @enderror" type="text" id="Nomor_KK" placeholder="Nomor KK"  required autofocus name="Nomor_KK"> 
+                @error('Nomor_KK') 
+                <div class="invalid-feedback">
+                {{ $message }}
+                </div>
+                @enderror
             </div>
+
             <div class="form-group mb-3">
                 <label for="Jenis_Kelamin">Jenis Kelamin</label>
-                <select class="form-control" id="Jenis_Kelamin" required autofocus name="Jenis_Kelamin">
+                <select class="form-control @error('Jenis_Kelamin') is-invalid @enderror" id="Jenis_Kelamin" required autofocus name="Jenis_Kelamin">
                     @php
                     foreach($JK as $jk){
                        echo('<option value="'. $jk. '"');
                         echo('>'.$jk. '</option>') ;
                     }
-                    @endphp
+                @endphp
                 </select>
+                @error('Jenis_Kelamin') 
+                <div class="invalid-feedback">
+                {{ $message }}
+                </div>
+                @enderror
             </div>
+
             <div class="form-group mb-3">
                 <label for="Status_Perkawinan">Status Perkawinan</label>
-                <select class="form-control" id="Status_Perkawinan" required autofocus name="Status_Perkawinan">
+                <select class="form-control @error('Status_Perkawinan') is-invalid @enderror" id="Status_Perkawinan" required autofocus name="Status_Perkawinan">
                     @php
                         foreach($statuses as $status){
                            echo('<option value="'. $status. '"');
@@ -46,18 +69,36 @@
                         }
                     @endphp
                 </select>
+                @error('Status_Perkawinan') 
+                <div class="invalid-feedback">
+                {{ $message }}
+                </div>
+                @enderror
             </div>
+
             <div class="form-group mb-3">
                 <label for="Tanggal_Lahir">Tanggal Lahir</label>
-                <input class="form-control" type="date" id="Tanggal_Lahir" required autofocus name="Tanggal_Lahir"> 
+                <input class="form-control @error('Tanggal_Lahir') is-invalid @enderror" type="date" id="Tanggal_Lahir" required autofocus name="Tanggal_Lahir"> 
+                @error('Tanggal_Lahir') 
+                <div class="invalid-feedback">
+                {{ $message }}
+                </div>
+                @enderror
             </div>
+
             <div class="form-group mb-3">
                 <label for="Pekerjaan">Pekerjaan</label>
-                <input class="form-control" type="text" id="Pekerjaan" placeholder="Pekerjaan" required autofocus name="Pekerjaan"> 
+                <input class="form-control @error('Pekerjaan') is-invalid @enderror" type="text" id="Pekerjaan" placeholder="Pekerjaan"  required autofocus name="Pekerjaan"> 
+                @error('Pekerjaan') 
+                <div class="invalid-feedback">
+                {{ $message }}
+                </div>
+                @enderror
             </div>
+
             <div class="form-group mb-3">
                 <label for="Status_Dalam_Keluarga">Status dalam Keluarga</label>
-                <select class="form-control" id="Status_Dalam_Keluarga" required autofocus name="Status_Dalam_Keluarga">
+                <select class="form-control @error('Status_Dalam_Keluarga') is-invalid @enderror" id="Status_Dalam_Keluarga" required autofocus name="Status_Dalam_Keluarga">
                     @php
                         foreach($kedudukans as $kedudukan){
                            echo('<option value="'. $kedudukan. '"');
@@ -65,14 +106,26 @@
                         }
                     @endphp
                 </select>
+                @error('Status_Dalam_Keluarga') 
+                <div class="invalid-feedback">
+                {{ $message }}
+                </div>
+                @enderror
             </div>
+
             <div class="form-group mb-3">
                 <label for="Nomor_Telepon">Nomor Telepon</label>
-                <input class="form-control" type="text" id="Nomor_Telepon" placeholder="Nomor Telepon" required autofocus name="Nomor_Telepon"> 
+                <input class="form-control @error('Nomor_Telepon') is-invalid @enderror" type="text" id="Nomor_Telepon" placeholder="Nomor Telepon" required autofocus name="Nomor_Telepon"> 
+                @error('Nomor_Telepon') 
+                <div class="invalid-feedback">
+                {{ $message }}
+                </div>
+                @enderror
             </div>
+
             <div class="form-group mb-3">
                 <label for="dusun_id">Dusun Tinggal</label>
-            <select class="form-control" id="dusun_id" required autofocus name="dusun_id">
+            <select class="form-control @error('dusun_id') is-invalid @enderror" id="dusun_id" required autofocus name="dusun_id">
                 @php
                     foreach($dusuns as $dusun){
                        echo('<option value="'. $dusun['DusunID']. '"');
@@ -80,9 +133,12 @@
                     }
                 @endphp
             </select>
+            @error('dusun_id') 
+            <div class="invalid-feedback">
+            {{ $message }}
             </div>
-
-
+            @enderror
+            </div>
             
             <div>
                 <button type="submit" class="btn btn-primary mb-3 justify-content-end">Submit</button>
